cc418fe2113bb023ce98e920d2253e2dc5406e1e
fuzii <ricardo.fuzii@yahoo.com.br>
2016-11-22 20:36:44 -0200

    dao

--- src/main/java/dao/ConnectionFactory.java
+++ src/main/java/dao/ConnectionFactory.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import org.postgresql.Driver;
 
 public class ConnectionFactory {
 	
