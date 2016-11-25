482c1d7c0b44abd34dba865afe57e005d5116db1
fuzii <ricardo.fuzii@yahoo.com.br>
2016-11-25 14:24:40 -0200

    Add files via upload

---
+++ src/main/java/servlet/SendGridEmail.java
@@ -0,0 +1,32 @@
+package geo;
+
+import com.sendgrid.*;
+import java.io.IOException;
+
+public class SendGridEmail {
+	
+	
+	public static void Send(String from, String to, String subject, String content) throws IOException{
+		
+		Mail mail = new Mail(new Email(from), subject, new Email(to), new Content("text/plain", content));
+
+		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		Request request = new Request();
+
+		try {
+
+			request.method = Method.POST;
+			request.endpoint = "mail/send";
+			request.body = mail.build();
+			Response response = sg.api(request);
+			System.out.println(response.statusCode);
+			System.out.println(response.body);
+			System.out.println(response.headers);
+
+		} catch (IOException ex) {
+			throw ex;
+		}
+
+	}
+
+}
