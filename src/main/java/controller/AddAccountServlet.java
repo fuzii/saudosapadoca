28cd4c6618d304f2cf6d39d2eb19b4794159a69e
fuzii <ricardo.fuzii@yahoo.com.br>
2016-11-29 18:04:22 -0200

    Update AddAccountServlet.java

--- src/main/java/controller/AddAccountServlet.java
+++ src/main/java/controller/AddAccountServlet.java
@@ -11,9 +11,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import dao.AccountDao;
+import dao.AddressDao;
+import dao.EstablishmentDao;
 import model.Account;
 import model.Address;
+import model.Establishment;
 import util.SendGridEmail;
 
 @WebServlet("/addAccount")
@@ -56,9 +63,79 @@ public class AddAccountServlet extends HttpServlet{
 		// response
 		response.addHeader("Access-Control-Allow-Origin","*");
 		response.addHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE");
-	    	response.addHeader("Access-Control-Max-Age","3600");
-	    	response.addHeader("Access-Control-Allow-Headers","x-requested-with");
-		response.sendRedirect("getEstablishmentByLocation?id="+address.getId());
+	    response.addHeader("Access-Control-Max-Age","3600");
+	    response.addHeader("Access-Control-Allow-Headers","x-requested-with");
+		
+	    //response.sendRedirect("getEstablishmentByLocation?id="+address.getId());
+	    
+//-----------------------------------------------------------------INICIO DO TESTE
+	    
+	    
+		// request
+		List<Address> addresses2 = EstablishmentDao.GetEstablishmentsAddressesByLocation(address);
+		
+		// response
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		
+		JSONArray jsonArrayEstablishment = new JSONArray();
+		
+		try {
+			
+			for(Address a : addresses2){
+				
+				Establishment establishment = EstablishmentDao.GetEstablishmentsById(a.getEstablishmentId());
+				
+				// establishment
+				JSONObject jsonProfile = new JSONObject();
+				jsonProfile.put("Id",establishment.getId());
+				jsonProfile.put("Name",establishment.getName());
+				jsonProfile.put("Alias",establishment.getAlias());
+				jsonProfile.put("RegisterNumber",establishment.getRegisterNumber());
+				
+				// address
+				JSONObject jsonAddress = new JSONObject();
+				jsonAddress.put("Id",a.getId());
+				jsonAddress.put("AccountId",a.getAccountId());
+				jsonAddress.put("EstablishmentId",a.getEstablishmentId());
+				jsonAddress.put("ZipCode",a.getZipCode());
+				jsonAddress.put("Street",a.getStreet());
+				jsonAddress.put("City",a.getCity());
+				jsonAddress.put("State",a.getState());
+				jsonAddress.put("Number;",a.getNumber()); 
+				jsonAddress.put("Premise",a.getPremise());
+				jsonAddress.put("Country",a.getCountry());
+				jsonAddress.put("Latitude",a.getLatitude());
+				jsonAddress.put("Longitude",a.getLongitude());
+				jsonAddress.put("Radius",a.getRadius());
+				
+				JSONObject jsonEstablishment = new JSONObject();
+				jsonEstablishment.put("address", jsonAddress);
+				jsonEstablishment.put("profile", jsonProfile);
+				
+				jsonArrayEstablishment.put(jsonEstablishment);
+
+			}
+			
+			JSONObject jsonEstablishments = new JSONObject();
+			jsonEstablishments.put("establishment", jsonArrayEstablishment);
+			
+			JSONObject jsonMain = new JSONObject();
+			jsonMain.put("establishments", jsonEstablishments);
+			
+			PrintWriter out = response.getWriter();
+			out.println(jsonMain.toString());
+			
+				
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//-----------------------------------------------------------------FIM DO TESTE
+	    
+	    
+	    
 		
 	}
 }
