f30aa8829f3f8bc91f8ea2f3624dc48f0016df5d
fuzii <ricardo.fuzii@yahoo.com.br>
2016-11-30 15:35:28 -0200

    Create GetFoursquareServlet.java

---
+++ src/main/java/controller/GetFoursquareServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Address;
+import util.Foursquare;
+
+@WebServlet("/getFoursquareServlet")
+public class GetFoursquareServlet extends HttpServlet{
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		// request
+		Address address = new Address();
+		address.setLatitude(Double.parseDouble(request.getParameter("latitude")));
+		address.setLongitude(Double.parseDouble(request.getParameter("longitude")));
+		
+		// response
+		PrintWriter out = response.getWriter();
+		out.print(Foursquare.GetFoursquareJSON(address).toString());
+		
+	}
+}
