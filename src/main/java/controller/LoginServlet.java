2a1c48aef1ee4bb66998c8cf148af31d0dc60a00
fuzii <ricardo.fuzii@yahoo.com.br>
2017-01-11 13:45:36 -0200

    Update LoginServlet.java

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -1,16 +1,23 @@
-	package controller;
+package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
-
+import util.Util;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import dao.AccountDao;
+import dao.EstablishmentDao;
 import dao.SessionDao;
 import dao.UserDao;
+import formatter.GenerateJSON;
 import model.User;
 
 @WebServlet("/login") 
@@ -41,7 +48,21 @@ public class LoginServlet extends HttpServlet{
 				response.addHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE");
 				response.addHeader("Access-Control-Max-Age","3600");
 				response.addHeader("Access-Control-Allow-Headers","x-requested-with");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("utf-8");
 				response.setStatus(HttpServletResponse.SC_OK);
+				
+				// response JSON
+				String establishment_id = String.valueOf(session.getAttribute("establishment_id")); 
+				String account_id = String.valueOf(session.getAttribute("account_id")); 
+				JSONObject jsonMain = new JSONObject();
+				PrintWriter out = response.getWriter();
+				out.print(jsonMain.put("user",GenerateJSON.GetUserJSON(user)));
+				if(!Util.IsEmpty(establishment_id))
+					out.print(jsonMain.put("establishment",GenerateJSON.GetEstablishmentJSON(EstablishmentDao.GetEstablishmentsById(Long.parseLong(establishment_id)))));
+				else
+					out.print(jsonMain.put("account",GenerateJSON.GetAccountJSON(AccountDao.GetAccountsById(Long.parseLong(account_id)))));
+				
 				
 			}else{
 				
